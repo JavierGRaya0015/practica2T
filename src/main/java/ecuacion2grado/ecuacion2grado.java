fb14bf8419dee308bb84b13989901cf5dba7d4fa
JAVIER GONZALEZ RAYA <9047@iespuertasdelcampo.es>
2020-03-10 12:12:00 +0100

    ecuacion segundo grado en blanco  dfgdgf

---
+++ src/main/java/ecuacion2grado/ecuacion2grado.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecuacion2grado;
+
+/**
+ *
+ * @author daw
+ */
+public class ecuacion2grado {
+    
+}
